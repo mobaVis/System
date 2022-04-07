1de337ae3b5820c7614a1995b68e60fa76586ac4
Linda <zhaochy1@shanghaitech.edu.cn>
2022-04-08 06:28:26 +0800

    add robot and animation

--- client/modules/threeJs/index.vue
+++ client/modules/threeJs/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="wrapper">
     <h1>this is detail page</h1>
+    <canvas id="helmet" width="800" height="600" />
   </div>
 </template>
 <script src="./script.js"></script>
--- client/modules/threeJs/script.js
+++ client/modules/threeJs/script.js
@@ -1,28 +1,36 @@
 // import HttpHelper from "common/utils/axios_helper.js";
-import { onMounted } from 'vue';
+import {
+    onMounted
+} from 'vue';
 import * as THREE from 'three';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls.js';
+import {
+    GLTFLoader
+} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {
+    RGBELoader
+} from 'three/examples/jsm/loaders/RGBELoader.js';
 export default {
     setup() {
-
         let camera, scene, renderer;
+        let model, mixer, clock;
 
         const init = () => {
+            const canvas = document.querySelector("#helmet");
+            // const container = document.createElement('div');
+            // document.body.appendChild(container);
 
-            const container = document.createElement( 'div' );
-            document.body.appendChild( container );
-
-            camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
-            camera.position.set( - 1.8, 0.6, 2.7 );
+            camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.25, 20);
+            camera.position.set(0, 5, 10);
 
             scene = new THREE.Scene();
 
             new RGBELoader()
-                .setPath( 'three/equirectangular/' )
-                .load( 'royal_esplanade_1k.hdr', function ( texture ) {
+                .setPath('three/equirectangular/')
+                .load('royal_esplanade_1k.hdr', function (texture) {
 
                     texture.mapping = THREE.EquirectangularReflectionMapping;
 
@@ -30,63 +38,82 @@ export default {
                     scene.environment = texture;
 
                     render();
-
                     // model
 
-                    const loader = new GLTFLoader().setPath( 'three/DamagedHelmet/glTF/' );
-                    loader.load( 'DamagedHelmet.gltf', function ( gltf ) {
+                    const loader = new GLTFLoader().setPath('three/calibur/');
+                    loader.load('scene.gltf', function (gltf) {
+                        model = gltf.scene;
 
-                        scene.add( gltf.scene );
+                        model.position.set(0, 0, 0);
+                        scene.add(model);
 
-                        render();
+                        // animate
+                        // add animations
+                        mixer = new THREE.AnimationMixer(model);
+                        let clip = THREE.AnimationClip.findByName(gltf.animations, "Walk");
+                        const action = mixer.clipAction(clip);
+                        action.play();
+                        animate();
+                        clock = new THREE.Clock();
+                    });
+                });
 
-                    } );
+            // var cloneModel = model.clone()
+            // cloneModel.position.set(-5, 0, 0);
+            // scene.add(cloneModel);
+            // render();
 
-                } );
 
-            renderer = new THREE.WebGLRenderer( { antialias: true } );
-            renderer.setPixelRatio( window.devicePixelRatio );
-            renderer.setSize( window.innerWidth, window.innerHeight );
+            renderer = new THREE.WebGLRenderer({
+                canvas,
+                antialias: true
+            });
+            renderer.setPixelRatio(window.devicePixelRatio);
+            // renderer.setSize(window.innerWidth, window.innerHeight);
             renderer.toneMapping = THREE.ACESFilmicToneMapping;
             renderer.toneMappingExposure = 1;
             renderer.outputEncoding = THREE.sRGBEncoding;
-            container.appendChild( renderer.domElement );
+            document.body.appendChild(renderer.domElement);
+            // container.appendChild(renderer.domElement);
 
-            const controls = new OrbitControls( camera, renderer.domElement );
-            controls.addEventListener( 'change', render ); // use if there is no animation loop
+            const controls = new OrbitControls(camera, renderer.domElement);
+            // controls.addEventListener('change', render); // use if there is no animation loop
             controls.minDistance = 2;
             controls.maxDistance = 10;
-            controls.target.set( 0, 0, - 0.2 );
+            controls.target.set(0, 0, -0.2);
             controls.update();
 
-            window.addEventListener( 'resize', onWindowResize );
+            canvas.addEventListener('resize', onWindowResize);
 
         };
 
         const onWindowResize = () => {
 
-            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.aspect = canvas.width / canvas.height;
             camera.updateProjectionMatrix();
-
-            renderer.setSize( window.innerWidth, window.innerHeight );
+            // renderer.setSize(window.innerWidth, window.innerHeight);
 
             render();
-
         };
 
         const render = () => {
-
-            renderer.render( scene, camera );
-
+            renderer.render(scene, camera);
         };
 
-        onMounted(() => {
-            init();
+        const animate = () => {
+            requestAnimationFrame(animate);
+            // update animate
+            if (clock) mixer.update(clock.getDelta());
             render();
+        }
+
+        onMounted(() => {
+            const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            init();
         });
 
         return {
-            
+
         };
     }
 };
